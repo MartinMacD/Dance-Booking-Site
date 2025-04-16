2cfaa75ee1d1f2ad36a7affb6b4e53dc5fa864f8
Martin <149177224+MartinMacD@users.noreply.github.com>
2025-04-16 12:35:41 +0100

    Added info box to landing, minor change in newclass

--- auth/auth.js
+++ auth/auth.js
@@ -1,16 +1,21 @@
-const bcrypt = require("bcrypt");
-const organiserModel = require("../models/organiserModel");
-const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt"); //Used for hashing passwords
+const organiserModel = require("../models/organiserModel"); //Data model
+const jwt = require("jsonwebtoken"); //Handles JWT tokens
 
+//Function to handle logins
 exports.login = function (req, res,next) {
+  //Username and password are received from the login form
     let username = req.body.username;
     let password = req.body.password;
   
+    //Check if organiser exists using username
     organiserModel.lookup(username, function (err, organiser) {
       if (err) {
+        //Couldn't perform lookup
         console.log("error looking up user", err);
         return res.status(401).send();
       }
+      //If organiser doesn't exist, display login page with message in log
       if (!organiser) {
         console.log("organiser ", username, " not found");
         return res.render("organiser/login", { loginFailed: true });
@@ -26,19 +31,23 @@ exports.login = function (req, res,next) {
           console.log("JWT cookie set:", accessToken);
           next();
         } else {
-          return res.render("organiser/login", { loginFailed: true }); //res.status(403).send();
+          return res.render("organiser/login", { loginFailed: true });
         }
       });
     });
   };
   
+  //Function to verify JWT tokens
   exports.verify = function (req, res, next) {
+    //Retrieve access token from cookie
     let accessToken = req.cookies.jwt;
     if (!accessToken) {
+      //If access token doesn't exist, reject request
       return res.status(403).send();
     }
     let payload;
     try {
+      //Verify token using the access token secret
       payload = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
       console.log("Payload:", payload);
       req.user = payload;
